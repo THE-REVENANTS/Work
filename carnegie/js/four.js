3eefe9b6cdd15128970d2132c18e24ab649ac5f7
Mr-montasir <mr.montasir.ma@gmail.com>
2022-11-29 18:05:33 +0100

    Added habbo and canergie websites

---
+++ carnegie/js/four.js
@@ -0,0 +1,1 @@
+dataLayer = window.dataLayer || [];
---
+++ carnegie/js/translate_options.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'en', 
+      includedLanguages: 'es,fr,de,nl,pt,it,cs,pl,en',
+    }, 'google_translate_element');
+  }
