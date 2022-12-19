b1e810d37e74e88247e72ff4a7dd57f842744690
Mr-montasir <mr.montasir.ma@gmail.com>
2022-12-19 19:52:21 +0100

    added more updates

--- social_App/assets/js/custom.js
+++ social_App/assets/js/custom.js
@@ -1,3 +1,6 @@
+function toggleHide_class(willToggle_hide) {
+    willToggle_hide.classList.toggle('hide');
+}
 // open privacy dropdown
 let privacy_icons = document.querySelectorAll('.privacy_icons');
 privacy_icons.forEach(e => {
@@ -20,7 +23,7 @@ post_privacy_options.forEach(e => {
 // Play and pause video 
 var play_video = document.querySelectorAll('.vid_container');
 // Event listener for the play/pause button
-play_video.forEach(e => {
+function playVideoFunction(e) {
     e.onclick = function () {
             let Video = e.firstElementChild;
             if (Video.paused) {
@@ -30,7 +33,8 @@ play_video.forEach(e => {
                 playIcon.style.display = "none";
             }
     }
-})
+}
+play_video.forEach(playVideoFunction);
 
 // My connections tabs control
 let connections_tab_control = document.querySelectorAll('.my_conntections_tab_control a');
@@ -59,3 +63,69 @@ let create_post_trigger = document.querySelector('#create_post_trigger');
 create_post_trigger.onclick = function () {
     modal_overlay.classList.remove('hide');
 }
+
+
+// My profile tabs PHOTOS/VIDEOS/FILES
+let profile_tab_control = document.querySelectorAll('.my_profile_tab_control a');
+let profile_tabs = document.querySelectorAll('.home-center-wrapper');
+let profile_container = document.querySelector('#profile');
+profile_tab_control.forEach(ClickVideoIcon);
+function ClickVideoIcon(e , index_num) {
+    e.onclick = function () {
+        if (e.classList.contains('full_width')) {
+            profile_container.classList.add('width_contain');
+        } else {
+            profile_container.classList.remove('width_contain')
+        }
+        for (let i = 0; i < profile_tab_control.length; i++) {
+            profile_tab_control[i].classList.remove('active');
+            e.classList.add('active');
+        }
+        // toggle tabs 
+        for (let t = 0; t < profile_tabs.length; t++) {
+            profile_tabs[t].classList.add('hide');
+            profile_tabs[index_num].classList.remove('hide');
+        }
+    }
+}
+
+let videos_modal_show_btn = document.querySelector('#videos_modal_show_btn');
+let upload_video_modal = document.querySelector('#upload_video_modal');
+let close_upload_video = document.querySelector('#close_upload_video'); 
+close_upload_video.setAttribute('onclick' , "toggleHide_class(upload_video_modal)");
+videos_modal_show_btn.setAttribute('onclick' , "toggleHide_class(upload_video_modal)");
+// file is too large modal event
+let file_is_too_large_modal = document.querySelector('#file_too_large');
+let close_file_is_too_large_modal = document.querySelector('#close_file_too_large');
+close_file_is_too_large_modal.setAttribute('onclick' , "toggleHide_class(file_is_too_large_modal)");
+let write_file_size = document.querySelector('#write_file_size');
+// file is too large modal ends... [open condition is inside loadVideo() function ]
+
+function floorFigure(figure, decimals){
+    if (!decimals) decimals = 2;
+    var d = Math.pow(10,decimals);
+    return (parseInt(figure*d)/d).toFixed(decimals);
+};
+let uploaded_videos = document.querySelector('#uploaded_videos');
+let upload_video_label = document.querySelector('#upload_video_label');
+var loadVideo = function(event) {
+    uploaded_videos.appendChild(upload_video_label);
+    upload_video_label.classList.add('grid_view');
+    let filess = event.target.files;
+    for (let i = 0; i < filess.length; i++) {
+        let file_size = floorFigure((filess[i].size * 0.00000095367432) , 2 );
+        if (file_size > 20) {
+            write_file_size.textContent = file_size;
+            file_is_too_large_modal.classList.remove('hide');
+            return false;
+        }
+        let video_container = document.createElement('div');
+        video_container.classList.add('vid_container');
+        let Uploaded_video = document.createElement('video');
+        Uploaded_video.src = URL.createObjectURL(filess[i]);
+            video_container.appendChild(Uploaded_video);
+            video_container.innerHTML += '<i class="fa-solid fa-play play_video"></i>';
+            uploaded_videos.appendChild(video_container);
+        playVideoFunction(video_container);
+    }
+};
--- social_App/assets/js/login.js
+++ social_App/assets/js/login.js
@@ -11,3 +11,19 @@ back_to_signin_link.addEventListener('click' , ()=> {
     sign_in_form.classList.remove('hide');
     forget_password_form.classList.add('hide');
 });
+
+let email_format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
+sign_in_form.addEventListener('submit' , (event)=> {
+    event.preventDefault();
+    let email_username = document.querySelector('#email_username').value;
+    let sign_in_alert = document.querySelector('.sign_in_form .alert');
+    if (sign_in_alert.classList.contains('blue')) {
+        sign_in_alert.classList.remove('blue');
+        sign_in_alert.classList.add('red');
+    }
+    if (email_username.toLowerCase().match(email_format)) {
+        sign_in_alert.innerHTML = '<strong>Error:</stong> the email <strong>'+email_username+'</strong> Is not registered on this site. If you are unsure of your email, try using your username instead';
+    } else {
+        sign_in_alert.innerHTML = '<strong>Error:</stong> the username <strong>'+email_username+'</strong> Is not registered on this site. If you are unsure of your username, try using your email instead';
+    }
+});
