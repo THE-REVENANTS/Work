f5d9ceeb808cf5c8fde7df43a5a3b4c986187947
Abdelmoula <selmani.abdelmoula.ma@gmail.com>
2023-01-01 17:01:13 +0100

    updated bitmeta

--- bitmeta/src/components/dashboard_components/left_sidebar.jsx
+++ bitmeta/src/components/dashboard_components/left_sidebar.jsx
@@ -4,22 +4,26 @@ function left_sidebar() {
     return (
         <div className="left_sidebar">
             <div className="top">
-                <div className="icon">
-                    <i class="fa-solid fa-sort"></i>
-                    Trade
+                <div className="icon active">
+                    <i class="fa-light fa-sort"></i>
+                    <span>Trade</span>
                 </div>
                 <div className="icon">
-                    <i class="fa-solid fa-share-nodes"></i>
-                    Vip member
+                    <i class="fa-light fa-share-nodes"></i>
+                    <span>Vip member</span>
                 </div>
                 <div className="icon">
-                    <i class="fa-solid fa-wallet"></i>
-                    Wallet
+                    <i class="fa-light fa-wallet"></i>
+                    <span>Wallet</span>
                 </div>
                 <div className="icon">
-                    <i class="fa-solid fa-gauge-min"></i>
-                    Dashboard
+                    <i class="fa-light fa-gauge-min"></i>
+                    <span>Dashboard</span>
                 </div>
+            </div>
+            <div className="bottom">
+                <i class="fa-light fa-arrow-right-from-bracket"></i>
+                <span>Logout</span>
             </div>
         </div>
     )
